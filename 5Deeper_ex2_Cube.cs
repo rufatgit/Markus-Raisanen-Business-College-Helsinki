247702e3c1264e0f0a957733fdc21443db431eda
rufatgit <93449554+rufatgit@users.noreply.github.com>
2023-03-11 23:55:19 +0100

    Update and rename Program.cs to 5Deeper_ex2_Cube.cs

--- 5Deeper_ex2_Cube.cs
+++ 5Deeper_ex2_Cube.cs
@@ -1,4 +1,17 @@
-﻿
+/*
+EXERCISE 5-2: Cube
+Create a Cube class that represents a cube.
+
+Create a public Cube (int edgeLength) constructor for the class that takes the length of the cube’s edge as its parameter.
+Make a public int Volume() method for the cube, which calculates and returns the cube’s volume. The volume of the cube is 
+calculated with the formula edgeLength * edgeLength * edgeLength.
+Moreover make a public override string ToString() method for the cube, which returns a string representation of it. 
+The string representation should take the form “The length of the edge is l and the volume v”. 
+Where l is the length and v the volume. Both the length and volume must be represented as integers.
+
+*/
+
+
 
 namespace ex_3_Cube
 {
