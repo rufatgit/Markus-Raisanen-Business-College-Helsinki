756f087893750dbf4fb6b73fac4c7f97bd405b92
rufatgit <93449554+rufatgit@users.noreply.github.com>
2023-03-12 00:01:49 +0100

    Update and rename Program.cs to 5Deeper_ex5_Overloaded_counter.cs

--- 5Deeper_ex5_Overloaded_counter.cs
+++ 5Deeper_ex5_Overloaded_counter.cs
@@ -1,4 +1,21 @@
-﻿
+/*
+EXERCISE 5-5: Overloaded Counter
+Implement a class called Counter. The class contains a number, whichs value can be increased and decreased. The class must have the following constructors.
+
+public Counter(int startValue) sets the start value of the counter to startValue.
+public Counter() sets the start value of the counter to 0.
+And the following methods and properties.
+
+public int value { get; set; }
+public void Increase() increases the value by 1
+public void Decrease() decreases the value by 1
+public void Increase(int increaseBy) increases the value of the counter by the value of increaseBy. If the value of increaseBy is negative,
+the value of the counter does not change.
+public void Decrease(int decreaseBy) decreases the value of the counter by the value of decreaseBy. If the value of decreaseBy is negative,
+the value of the counter does not change.
+
+*/
+
 
 namespace ex_5_Overloaded_counter
 {
